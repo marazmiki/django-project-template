26b6855ba1f029b99889ec5707b4f90688ce9531
Michael Porokhovnichenko <marazmiki@gmail.com>
2012-10-28 14:55:11 +0700

    Added TMEPLATE_CONTEXT_PROCESSORS into settings template (close #1); added compiled loaders for production mode

--- src/project/settings/__init__.py
+++ src/project/settings/__init__.py
@@ -58,10 +58,24 @@ STATICFILES_FINDERS = (
 ##
 ## Templates settings
 ##
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+TEMPLATE_LOADERS =  (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
 TEMPLATE_DIRS = (
     rel('src/project/templates/'),
 )
